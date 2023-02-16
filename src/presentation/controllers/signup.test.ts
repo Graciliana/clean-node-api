31a4e76af2306ee48eb53ad3ba136bd7b1218bc5
Graciliana Kascher <eagraciliana@gmail.com>
2023-02-16 00:30:49 -0300

    test: ensure SignUpController returns 200 sucess

---
+++ src/presentation/controllers/signup.test.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from "./signup"
+
+describe('Signup Controller', () => {
+  
+  test('Validar login com sucesso', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+  })
+})
