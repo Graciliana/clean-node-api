94df1598169cf4ede860557c1d15a4544bc66f4e
Graciliana Kascher <eagraciliana@gmail.com>
2023-02-19 09:30:46 -0300

    test: ensure SignUpController returns 401 missing param email and email

--- src/presentation/controllers/signup.test.ts
+++ src/presentation/controllers/signup.test.ts
@@ -1,18 +1,29 @@
 import { SignUpController } from "./signup"
-
 describe('Signup Controller', () => {
-  
-  test('Validar login com sucesso', () => {
+  test('Validar login sem passar o nome ', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        name: 'any_name',
         email: 'any_email@mail.com',
         password: 'any_password',
         passwordConfirmation: 'any_password',
       },
     }
     const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.statusCode).toBe(401)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+  test('Validar login sem passar o email ', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name', 
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(401)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
