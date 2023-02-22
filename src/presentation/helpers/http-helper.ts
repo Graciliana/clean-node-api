5ab4e47499d1837aac0b81f0f725968ea2aa78a6
Graciliana Kascher <eagraciliana@gmail.com>
2023-02-22 13:27:48 -0300

    refactor: simplify code

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,5 @@
 import { HttpResponse } from './../protocols/http'
-import { MissingParamError } from './../errors/missing-param-error'
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 401,
-  body: error,
+  body: error
 })
